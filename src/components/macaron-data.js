3eca1b6325395a8407ecc03cf448441c984b4071
Duy Nguyen <duyng93@gmail.com>
2018-07-17 17:32:39 -0700

    add macaron data into an array of objects

---
+++ src/components/macaron-data.js
@@ -0,0 +1,100 @@
+import birthdayCake from '../assets/images/flavor/birthday-cake.jpeg';
+import caramelChocolate from '../assets/images/flavor/caramel_chocolat.jpg';
+import chocolate from '../assets/images/flavor/chocolate-macaron.jpg';
+import cookiesCream from '../assets/images/flavor/cookies-cream.jpg';
+import earlGrey from '../assets/images/flavor/earl-grey-jasmine.jpg';
+import jasmine from '../assets/images/flavor/jasmine-green-tea.jpg';
+import lavender from '../assets/images/flavor/lavender-cream.jpg';
+import lemon from '../assets/images/flavor/lemon-sorbet.jpg';
+import matcha from '../assets/images/flavor/matcha-macarons.jpg';
+import strawberry from '../assets/images/flavor/strawberry-rose.jpg';
+import orange from '../assets/images/flavor/orange-butter.jpg';
+import redWine from '../assets/images/flavor/red-wine.jpg';
+import saltedCaramel from '../assets/images/flavor/salted-caramel.png';
+import tiramisu from '../assets/images/flavor/tiramisu.jpg';
+import coconut from '../assets/images/flavor/toasted-coconut.jpeg';
+import ube from '../assets/images/flavor/ube-cream.jpg';
+
+
+export default [
+    {
+        src: birthdayCake,
+        name: "Birthday Cake",
+        description: "this is a Birthday Cake flavor"
+    },
+    {
+        src: caramelChocolate,
+        name: "Caramel Chocolate",
+        description: "this is a Caramel Chocolate flavor"
+    },
+    {
+        src: chocolate,
+        name: "Chocolate",
+        description: "this is a chocolate flavor"
+    },
+    {
+        src: cookiesCream,
+        name: "Cookies Cream",
+        description: "this is a Cookies Cream flavor"
+    },
+    {
+        src: earlGrey,
+        name: "Earl Grey Jasmine",
+        description: "this is a Earl Grey Jasmine flavor"
+    },
+    {
+        src: jasmine,
+        name: "Jasmine Green Tea",
+        description: "this is a Jasmine Green Tea flavor"
+    },
+    {
+        src: lavender,
+        name: "Lavender Cream",
+        description: "this is a Lavender Cream flavor"
+    },
+    {
+        src: lemon,
+        name: "Lemon Sorbet",
+        description: "this is a Lemon Sorbet flavor"
+    },
+    {
+        src: matcha,
+        name: "Japanese Matcha",
+        description: "this is a Japanese Matcha flavor"
+    },
+    {
+        src: strawberry,
+        name: "Strawberry Rose",
+        description: "this is a Strawberry Rose flavor"
+    },
+    {
+        src: orange,
+        name: "Orange Butter",
+        description: "this is a Orange Butter flavor"
+    },    
+    {
+        src: redWine,
+        name: "Red Wine",
+        description: "this is a Red Wine flavor"
+    },
+    {
+        src: saltedCaramel,
+        name: "Salted Caramel",
+        description: "this is a Salted Caramel flavor"
+    },
+    {
+        src: tiramisu,
+        name: "Tiramisu",
+        description: "this is a Tiramisu flavor"
+    },
+    {
+        src: coconut,
+        name: "Toasted Coconut",
+        description: "this is a Toasted Coconut flavor"
+    },
+    {
+        src: ube,
+        name: "Ube Cream",
+        description: "this is a Ube Cream flavor"
+    }
+]
